fb4135ef8811ee0308d652ecc454b748bebbcb2c
Almaz <almaz.khannanov@gmail.com>
2023-04-08 17:35:27 +0200

    Added button + nextauth configuration

---
+++ lib/auth.ts
@@ -0,0 +1,63 @@
+import { NextAuthOptions } from "next-auth";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+import { db } from "./db";
+import GoogleProvider from "next-auth/providers/google";
+
+function geGoogleCredentials() {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+  if (!clientId || !clientSecret) {
+    throw new Error(
+      "Google client ID or secret not found. Please add them to your .env.local file."
+    );
+  }
+
+  return { clientId, clientSecret };
+}
+
+export const authOptions: NextAuthOptions = {
+  adapter: UpstashRedisAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: geGoogleCredentials().clientId,
+      clientSecret: geGoogleCredentials().clientSecret,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      const dbUser = (await db.get(`user:${user.id}`)) as User | null;
+
+      if (!dbUser) {
+        token.id = user!.id;
+      }
+
+      return {
+        id: dbUser?.id || token.id,
+        name: dbUser?.name,
+        email: dbUser?.email,
+        image: dbUser?.image,
+      };
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user = {
+          id: token.id,
+          name: token.name,
+          email: token.email,
+        };
+      }
+
+      return session;
+    },
+    redirect() {
+      return "/dashboard";
+    },
+  },
+};
